b3b2aaa0ee7e66e00318b2df81805da169bc7953
shahan953 <misbauddin1994@gmail.com>
2022-06-14 19:34:46 +0600

    Readme updated

---
+++ README.md
@@ -0,0 +1,25 @@
+# GitHub/EC2 Instance | Automatic Deployment using GitHub Action
+
+This is a simple express server where the main focus is to deploy application in AWS EC2 instance automatically. 
+
+## prerequisition
+We need few things to know to deploy application automatically in EC2 or any other cloud server
+
+- Github account
+- AWS or any other cloud server
+- Any application (I used nodejs-express server)
+
+So let's dive in
+
+The steps that we will follow to reach our goal
+
+- Create one ec2 instance in aws console
+- Install nodejs in aws 
+- Create a github repository
+- Push our code in git repo
+- Setup GitHub Workflow file
+- Setup Action Runners
+- Connect Action Runner with ec2 instance
+- Run Action Runner
+
+...Wola we have completed our task
--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/test', (req, res) => {
-  res.send("Action runner updated")
-})
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
+app.get('/test', (req, res) => {
+  res.send('Action runner updated');
+});
 
 app.get('/api/v1/user', (req, res) => {
   res.json({
     success: true,
-    message: "User fetched",
-    company: {
-      name: "Hello company"
-    }
-  })
-})
+    message: 'User fetched',
+    user: {
+      fullname: 'Hello company',
+      email: "user@gmail.com",
+      phone: "+880293938383"
+    },
+  });
+});
 
-const port = 8080
-app.listen(port, () => console.log(`App listening port ${port}`))
+
+
+const port = 8080;
+app.listen(port, () => console.log(`App listening port ${port}`));
